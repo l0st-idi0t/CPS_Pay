07b0da245c8da4bf8a9b9138b43d72a2795ed5be
l0st-idi0t <carlnod121@gmail.com>
2023-03-19 23:57:09 -0500

    turning this into an API

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
+from flask import Flask, jsonify, render_template, Response
+from threading import Thread
+from flask_restful import Resource, Api
 import xlrd
 import requests
 
-
-def request_salaries():
-    url = "https://www.cps.edu/about/finance/employee-position-files/"
-    response = str(requests.get(url).content)
-
-    #find most recent salary file
-    filePos = response.find("globalassets/cps-pages/about-cps/finance/employee-position-files/")
-    fileURL = "https://www.cps.edu/" + response[filePos : filePos + 100]
-    fileName = response[filePos + 65 : filePos + 100]
-
-    #download file
-    r = requests.get(fileURL)
-    with open(fileName, 'wb') as f:
-        f.write(r.content)
-
-    return fileName
+app = Flask(__name__)
+api = Api(app)
 
 
-def main():
-    wb_file = request_salaries()
-    wb = xlrd.open_workbook(wb_file)
+def run():
+  app.run(host='0.0.0.0', port=8888)
 
-    sheet = wb.sheet_by_index(0)
+
+class Empty(Resource):
+    
+        def __init__(self):
+            pass
+    
+        def get(self):
+            return {
+                "Hello": "World"
+            }
+
+
+class Info(Resource):
+
+    def __init__(self):
+        self.fileName = self.request_salaries()
+
+    def get(self, school, lastName, job, fte):
+        return {
+            "Hello": "World"
+        }
+
+    def request_salaries(self):
+        url = "https://www.cps.edu/about/finance/employee-position-files/"
+        response = str(requests.get(url).content)
+
+        #find most recent salary file
+        filePos = response.find("globalassets/cps-pages/about-cps/finance/employee-position-files/")
+        fileURL = "https://www.cps.edu/" + response[filePos : filePos + 100]
+        fileName = response[filePos + 65 : filePos + 100]
+
+        #download file
+        r = requests.get(fileURL)
+        with open(fileName, 'wb') as f:
+            f.write(r.content)
+
+        return fileName
+
+
+#api endpoint
+api.add_resource(Empty, '/')
+api.add_resource(Info, '/<string:school>/<string:lastName>/<string:job>/<float:fte>')
 
 
 if __name__ == '__main__':
-    main()
+    t = Thread(target=run)
+    t.start()
